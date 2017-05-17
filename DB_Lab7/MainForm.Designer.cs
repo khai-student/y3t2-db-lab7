aa75d1a0db98e43c6962e25c2c1b135c1121598a
Your Name <you@example.com>
2017-05-17 07:19:51 +0300

    Init commit.

---
+++ DB_Lab7/MainForm.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace DB_Lab6
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.SplitContainer splitContainer1;
+            System.Windows.Forms.GroupBox groupBox1;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.guiSearchGroupBox = new System.Windows.Forms.GroupBox();
+            this.guiSearchColumn = new System.Windows.Forms.ComboBox();
+            this.guiSearhPattern = new System.Windows.Forms.TextBox();
+            this.guiSearch = new System.Windows.Forms.Button();
+            this.guiTables = new System.Windows.Forms.ComboBox();
+            this.guiUpdateTable = new System.Windows.Forms.Button();
+            this.guiQueryTable = new System.Windows.Forms.Button();
+            this.guiDGV = new System.Windows.Forms.DataGridView();
+            this.guiFilterGroupBox = new System.Windows.Forms.GroupBox();
+            this.guiFilterColumn = new System.Windows.Forms.ComboBox();
+            this.guiFilter = new System.Windows.Forms.Button();
+            this.guiFilterDirection = new System.Windows.Forms.ComboBox();
+            label1 = new System.Windows.Forms.Label();
+            splitContainer1 = new System.Windows.Forms.SplitContainer();
+            groupBox1 = new System.Windows.Forms.GroupBox();
+            ((System.ComponentModel.ISupportInitialize)(splitContainer1)).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            this.guiSearchGroupBox.SuspendLayout();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guiDGV)).BeginInit();
+            this.guiFilterGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(6, 30);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(34, 13);
+            label1.TabIndex = 1;
+            label1.Text = "Table";
+            // 
+            // splitContainer1
+            // 
+            splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            splitContainer1.Location = new System.Drawing.Point(0, 0);
+            splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            splitContainer1.Panel1.Controls.Add(this.guiFilterGroupBox);
+            splitContainer1.Panel1.Controls.Add(this.guiSearchGroupBox);
+            splitContainer1.Panel1.Controls.Add(groupBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            splitContainer1.Panel2.Controls.Add(this.guiDGV);
+            splitContainer1.Size = new System.Drawing.Size(783, 485);
+            splitContainer1.SplitterDistance = 180;
+            splitContainer1.SplitterWidth = 3;
+            splitContainer1.TabIndex = 100;
+            // 
+            // guiSearchGroupBox
+            // 
+            this.guiSearchGroupBox.Controls.Add(this.guiSearchColumn);
+            this.guiSearchGroupBox.Controls.Add(this.guiSearhPattern);
+            this.guiSearchGroupBox.Controls.Add(this.guiSearch);
+            this.guiSearchGroupBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guiSearchGroupBox.Enabled = false;
+            this.guiSearchGroupBox.Location = new System.Drawing.Point(0, 83);
+            this.guiSearchGroupBox.Name = "guiSearchGroupBox";
+            this.guiSearchGroupBox.Size = new System.Drawing.Size(180, 110);
+            this.guiSearchGroupBox.TabIndex = 5;
+            this.guiSearchGroupBox.TabStop = false;
+            this.guiSearchGroupBox.Text = "Search";
+            // 
+            // guiSearchColumn
+            // 
+            this.guiSearchColumn.FormattingEnabled = true;
+            this.guiSearchColumn.Location = new System.Drawing.Point(6, 19);
+            this.guiSearchColumn.Name = "guiSearchColumn";
+            this.guiSearchColumn.Size = new System.Drawing.Size(166, 21);
+            this.guiSearchColumn.TabIndex = 0;
+            // 
+            // guiSearhPattern
+            // 
+            this.guiSearhPattern.Location = new System.Drawing.Point(6, 46);
+            this.guiSearhPattern.Name = "guiSearhPattern";
+            this.guiSearhPattern.Size = new System.Drawing.Size(166, 20);
+            this.guiSearhPattern.TabIndex = 3;
+            // 
+            // guiSearch
+            // 
+            this.guiSearch.Location = new System.Drawing.Point(6, 72);
+            this.guiSearch.Name = "guiSearch";
+            this.guiSearch.Size = new System.Drawing.Size(166, 23);
+            this.guiSearch.TabIndex = 4;
+            this.guiSearch.Text = "Search";
+            this.guiSearch.UseVisualStyleBackColor = true;
+            this.guiSearch.Click += new System.EventHandler(this.guiSearch_Click);
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(label1);
+            groupBox1.Controls.Add(this.guiTables);
+            groupBox1.Controls.Add(this.guiUpdateTable);
+            groupBox1.Controls.Add(this.guiQueryTable);
+            groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            groupBox1.Location = new System.Drawing.Point(0, 0);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new System.Drawing.Size(180, 83);
+            groupBox1.TabIndex = 5;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Table";
+            // 
+            // guiTables
+            // 
+            this.guiTables.FormattingEnabled = true;
+            this.guiTables.Location = new System.Drawing.Point(51, 27);
+            this.guiTables.Name = "guiTables";
+            this.guiTables.Size = new System.Drawing.Size(121, 21);
+            this.guiTables.TabIndex = 0;
+            // 
+            // guiUpdateTable
+            // 
+            this.guiUpdateTable.Enabled = false;
+            this.guiUpdateTable.Location = new System.Drawing.Point(90, 54);
+            this.guiUpdateTable.Name = "guiUpdateTable";
+            this.guiUpdateTable.Size = new System.Drawing.Size(82, 23);
+            this.guiUpdateTable.TabIndex = 4;
+            this.guiUpdateTable.Text = "Update table";
+            this.guiUpdateTable.UseVisualStyleBackColor = true;
+            this.guiUpdateTable.Click += new System.EventHandler(this.guiUpdateTable_Click);
+            // 
+            // guiQueryTable
+            // 
+            this.guiQueryTable.Location = new System.Drawing.Point(9, 54);
+            this.guiQueryTable.Name = "guiQueryTable";
+            this.guiQueryTable.Size = new System.Drawing.Size(75, 23);
+            this.guiQueryTable.TabIndex = 4;
+            this.guiQueryTable.Text = "Query table";
+            this.guiQueryTable.UseVisualStyleBackColor = true;
+            this.guiQueryTable.Click += new System.EventHandler(this.guiQueryTable_Click);
+            // 
+            // guiDGV
+            // 
+            this.guiDGV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.guiDGV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.guiDGV.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.guiDGV.Location = new System.Drawing.Point(0, 0);
+            this.guiDGV.MultiSelect = false;
+            this.guiDGV.Name = "guiDGV";
+            this.guiDGV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.guiDGV.Size = new System.Drawing.Size(600, 485);
+            this.guiDGV.TabIndex = 10;
+            this.guiDGV.RowsRemoved += new System.Windows.Forms.DataGridViewRowsRemovedEventHandler(this.guiDGV_RowsRemoved);
+            // 
+            // guiFilterGroupBox
+            // 
+            this.guiFilterGroupBox.Controls.Add(this.guiFilterDirection);
+            this.guiFilterGroupBox.Controls.Add(this.guiFilterColumn);
+            this.guiFilterGroupBox.Controls.Add(this.guiFilter);
+            this.guiFilterGroupBox.Dock = System.Windows.Forms.DockStyle.Top;
+            this.guiFilterGroupBox.Enabled = false;
+            this.guiFilterGroupBox.Location = new System.Drawing.Point(0, 193);
+            this.guiFilterGroupBox.Name = "guiFilterGroupBox";
+            this.guiFilterGroupBox.Size = new System.Drawing.Size(180, 110);
+            this.guiFilterGroupBox.TabIndex = 6;
+            this.guiFilterGroupBox.TabStop = false;
+            this.guiFilterGroupBox.Text = "Filter";
+            // 
+            // guiFilterColumn
+            // 
+            this.guiFilterColumn.FormattingEnabled = true;
+            this.guiFilterColumn.Location = new System.Drawing.Point(6, 19);
+            this.guiFilterColumn.Name = "guiFilterColumn";
+            this.guiFilterColumn.Size = new System.Drawing.Size(166, 21);
+            this.guiFilterColumn.TabIndex = 0;
+            // 
+            // guiFilter
+            // 
+            this.guiFilter.Location = new System.Drawing.Point(6, 72);
+            this.guiFilter.Name = "guiFilter";
+            this.guiFilter.Size = new System.Drawing.Size(166, 23);
+            this.guiFilter.TabIndex = 4;
+            this.guiFilter.Text = "Filter";
+            this.guiFilter.UseVisualStyleBackColor = true;
+            this.guiFilter.Click += new System.EventHandler(this.guiFilter_Click);
+            // 
+            // guiFilterDirection
+            // 
+            this.guiFilterDirection.FormattingEnabled = true;
+            this.guiFilterDirection.Items.AddRange(new object[] {
+            "ascending",
+            "descending"});
+            this.guiFilterDirection.Location = new System.Drawing.Point(6, 45);
+            this.guiFilterDirection.Name = "guiFilterDirection";
+            this.guiFilterDirection.Size = new System.Drawing.Size(166, 21);
+            this.guiFilterDirection.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(783, 485);
+            this.Controls.Add(splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Hospital DB - Lab6";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(splitContainer1)).EndInit();
+            splitContainer1.ResumeLayout(false);
+            this.guiSearchGroupBox.ResumeLayout(false);
+            this.guiSearchGroupBox.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guiDGV)).EndInit();
+            this.guiFilterGroupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox guiTables;
+        private System.Windows.Forms.DataGridView guiDGV;
+        private System.Windows.Forms.TextBox guiSearhPattern;
+        private System.Windows.Forms.Button guiSearch;
+        private System.Windows.Forms.Button guiQueryTable;
+        private System.Windows.Forms.ComboBox guiSearchColumn;
+        private System.Windows.Forms.Button guiUpdateTable;
+        private System.Windows.Forms.GroupBox guiSearchGroupBox;
+        private System.Windows.Forms.GroupBox guiFilterGroupBox;
+        private System.Windows.Forms.ComboBox guiFilterColumn;
+        private System.Windows.Forms.Button guiFilter;
+        private System.Windows.Forms.ComboBox guiFilterDirection;
+    }
+}
+
---
+++ DB_Lab7/MainForm.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DB_Lab6
+{
+    public partial class MainForm : Form
+    {
+        private PostgresDatabase _db = new PostgresDatabase();
+        private List<string> _ids = new List<string>();
+        private bool _is_table_updating = false;
+        private List<List<string>> table = null;
+        private string _sql = "";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            GetTablesList();
+        }
+
+        public void GetTablesList()
+        {
+            if (_is_table_updating)
+            {
+                return;
+            }
+            _is_table_updating = true;
+
+            _sql =    "SELECT table_name " +
+                            "FROM information_schema.tables " +
+                            "WHERE table_schema = 'public' " +
+                                    "AND table_type = 'BASE TABLE' " +
+                            "ORDER BY table_name;";
+
+            List<List<string>> tables = _db.QueryList(_sql);
+            guiTables.Items.Clear();
+            for (int row = 1; row < tables.Count; row++)
+            {
+                for (int col = 0; col < tables[row].Count; col++)
+                {
+                    guiTables.Items.Add(tables[row][col]);
+                }
+            }
+
+            guiTables.SelectedIndex = 0;
+
+            _is_table_updating = false;
+        }
+
+        /// <summary>
+        /// If sender == null and e == null then old values from this.tabel will be used.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void guiQueryTable_Click(object sender, EventArgs e)
+        {
+            if (_is_table_updating)
+            {
+                return;
+            }
+            _is_table_updating = true;
+
+            guiUpdateTable.Enabled = true;
+            guiSearchGroupBox.Enabled = true;
+            guiFilterGroupBox.Enabled = true;
+
+            // if not called manually
+            if (!(sender == null && e == null))
+            {
+                _sql = string.Format("SELECT * FROM {0};", guiTables.SelectedItem.ToString());
+                table = _db.QueryList(_sql);
+            }
+
+            guiDGV.Rows.Clear();
+            guiDGV.Columns.Clear();
+
+            if (table.Count == 0)
+            {
+                return;
+            }
+            // adding columns
+            guiSearchColumn.Items.Clear();
+            guiFilterColumn.Items.Clear();
+            guiSearhPattern.Clear();
+
+            for (int col_index = 0; col_index < table[0].Count; col_index++)
+            {   // in first row there always column headers
+                guiDGV.Columns.Add(table[0][col_index], table[0][col_index]);
+
+                if (table[0][col_index].ToLower() == "id")
+                {
+                    guiDGV.Columns[col_index].Visible = false;
+                }
+                else
+                {
+                    guiSearchColumn.Items.Add(table[0][col_index]);
+                    guiFilterColumn.Items.Add(table[0][col_index]);
+                }
+            }
+            // managing search elements
+            guiSearchColumn.SelectedIndex = 0;
+            guiFilterColumn.SelectedIndex = 0;
+            guiFilterDirection.SelectedIndex = 0;
+
+            // filling table
+            _ids.Clear();
+            for (int row = 1; row < table.Count; row++)
+            {
+                guiDGV.Rows.Add();
+                for (int col = 0; col < table[row].Count; col++)
+                {
+                    if (table[row][col] == "True" || table[row][col] == "False")
+                    {
+                        guiDGV.Rows[row - 1].Cells[col] = new DataGridViewCheckBoxCell(false);
+                        guiDGV.Rows[row - 1].Cells[col].Value = table[row][col] == "True";
+                    }
+                    else
+                    {
+                        guiDGV.Rows[row - 1].Cells[col].Value = table[row][col];
+                    }
+
+                    if (table[0][col] == "id")
+                    {
+                        _ids.Add(table[row][col]);
+                    }
+                }
+            }
+
+            _is_table_updating = false;
+        }
+
+        private void guiUpdateTable_Click(object sender, EventArgs e)
+        {
+            if (_is_table_updating)
+            {
+                return;
+            }
+            _is_table_updating = true;
+
+            for (int row = 0; row < guiDGV.Rows.Count-1; row++)
+            {
+                // if record is new
+                if (guiDGV.Rows[row].Cells["id"].Value == null)
+                {
+                    InsertRow(row);
+                    continue;
+                }
+
+                _sql = string.Format("UPDATE {0} SET ", guiTables.SelectedItem.ToString());
+
+                for (int col = 0; col < guiDGV.Rows[row].Cells.Count; col++)
+                {
+                    // dont touch id
+                    if (guiDGV.Columns[col].HeaderText == "id" || guiDGV.Rows[row].Cells[col].Value == null)
+                    {
+                        continue;
+                    }
+
+                    if (guiDGV.Rows[row].Cells[col] is DataGridViewCheckBoxCell)
+                    {
+                        _sql += string.Format("{0} = {1}",
+                                guiDGV.Rows[row].Cells[col].OwningColumn.HeaderText,
+                                (bool)guiDGV.Rows[row].Cells[col].Value ? "TRUE" : "FALSE");
+                    }
+                    else
+                    {
+                        double number = 0;
+                        if (double.TryParse(guiDGV.Rows[row].Cells[col].Value.ToString(), out number))
+                        {
+                            _sql += string.Format("{0} = {1}",
+                                guiDGV.Rows[row].Cells[col].OwningColumn.HeaderText,
+                                guiDGV.Rows[row].Cells[col].Value.ToString());
+                        }
+                        else
+                        {
+                            _sql += string.Format("{0} = '{1}'",
+                                guiDGV.Rows[row].Cells[col].OwningColumn.HeaderText,
+                                guiDGV.Rows[row].Cells[col].Value.ToString());
+                        }
+                    }
+                    // adding space and ,
+                    _sql += ", ";
+                }
+
+                _sql += string.Format("WHERE id = {0};", guiDGV.Rows[row].Cells["id"].Value.ToString());
+                _sql = _sql.Replace(", WHERE", " WHERE");
+
+                // trying to update
+                try
+                {
+                    _db.Exec(_sql);
+                }
+                catch (Exception ex)
+                {
+                    guiDGV.ClearSelection();
+                    guiDGV.Rows[row].Selected = true;
+                    Logger.Error("Cannot update selected cortege.\n" + ex.Message);
+                    break;
+                }
+            }
+
+            _is_table_updating = false;
+        }
+
+        private void InsertRow(int row)
+        {
+            if (row < 0)
+            {
+                throw new IndexOutOfRangeException();
+            }
+
+            _sql = string.Format("INSERT INTO {0} (", guiTables.SelectedItem.ToString());
+
+            for (int col = 0; col < guiDGV.Rows[row].Cells.Count; col++)
+            {
+                // dont touch id
+                if (guiDGV.Columns[col].HeaderText != "id")
+                {
+                    _sql += string.Format("{0}, ", guiDGV.Columns[col].HeaderText);
+                }
+            }
+
+            _sql += ") VALUES (";
+            _sql = _sql.Replace(", )", ")");
+
+            for (int col = 0; col < guiDGV.Rows[row].Cells.Count; col++)
+            {
+                // dont touch id
+                if (guiDGV.Columns[col].HeaderText != "id")
+                {
+                    if (guiDGV.Rows[row].Cells[col] is DataGridViewCheckBoxCell)
+                    {
+                        _sql += (bool)guiDGV.Rows[row].Cells[col].Value ? "TRUE" : "FALSE";
+                    }
+                    else
+                    {
+                        double number = 0;
+                        if (double.TryParse(guiDGV.Rows[row].Cells[col].Value.ToString(), out number))
+                        {
+                            _sql += guiDGV.Rows[row].Cells[col].Value.ToString();
+                        }
+                        else
+                        {
+                            _sql += string.Format("'{0}'", guiDGV.Rows[row].Cells[col].Value.ToString());
+                        }
+                    }
+                    // adding space and ,
+                    _sql += ", ";
+                }
+            }
+
+            _sql += ");";
+            _sql = _sql.Replace(", )", ")");
+
+            // trying to insert
+            try
+            {
+                _db.Exec(_sql);
+            }
+            catch (Exception ex)
+            {
+                guiDGV.ClearSelection();
+                guiDGV.Rows[row].Selected = true;
+                Logger.Error("Cannot insert selected cortege.\n" + ex.Message);
+            }
+        }
+
+        private void guiDGV_RowsRemoved(object sender, DataGridViewRowsRemovedEventArgs e)
+        {
+            if (_is_table_updating)
+            {
+                return;
+            }
+            _is_table_updating = true;
+
+            try
+            {
+                _db.Exec(string.Format("DELETE FROM {0} WHERE id = {1};",
+                guiTables.SelectedItem.ToString(),
+                _ids[e.RowIndex]));
+            }
+            catch (Exception ex)
+            {
+                Logger.Error("Failed to remove the row.\n" + ex.Message);
+            };
+
+            _is_table_updating = false;
+        }
+
+        private void guiSearch_Click(object sender, EventArgs e)
+        {
+            if (_is_table_updating)
+            {
+                return;
+            }
+            _is_table_updating = true;
+
+            string pattern = _db.SecureString(guiSearhPattern.Text);
+            if (pattern == "")
+            {
+                _is_table_updating = false;
+                return;
+            }
+
+            _sql = "";
+            double number = 0;
+            
+            if (pattern.ToLower() == "true" || pattern.ToLower() == "false")
+            {
+                _sql = string.Format("SELECT * FROM {0} WHERE {1} = {2};",
+                guiTables.SelectedItem.ToString(),
+                guiSearchColumn.SelectedItem.ToString(),
+                pattern.ToUpper());
+            }
+            else if (double.TryParse(pattern, out number))
+            {
+                _sql = string.Format("SELECT * FROM {0} WHERE {1} = {2};",
+                guiTables.SelectedItem.ToString(),
+                guiSearchColumn.SelectedItem.ToString(),
+                pattern);
+                
+            }
+            else
+            {
+                _sql = string.Format("SELECT * FROM {0} WHERE {1} ~ '.*{2}.*';",
+                guiTables.SelectedItem.ToString(),
+                guiSearchColumn.SelectedItem.ToString(),
+                pattern);
+            }
+
+            // trying to select
+            try
+            {
+                table = _db.QueryList(_sql);
+
+                _is_table_updating = false;
+                guiQueryTable_Click(null, null);
+                _is_table_updating = true;
+            }
+            catch
+            {
+                Logger.Info("No items found.");
+            }
+            finally
+            {
+                _is_table_updating = false;
+            }
+        }
+
+        private void guiFilter_Click(object sender, EventArgs e)
+        {
+            if (_is_table_updating)
+            {
+                return;
+            }
+            _is_table_updating = true;
+
+            string order_sql = _sql;
+            if (order_sql.IndexOf("SELECT") < 0)
+            {
+                order_sql = string.Format("SELECT * FROM {0};", guiTables.SelectedItem.ToString());
+            }
+            // 
+            string direction = guiFilterDirection.SelectedItem.ToString() == "ascending" ? "ASC" : "DESC";
+            order_sql += string.Format(" ORDER BY {0} {1};", guiFilterColumn.SelectedItem.ToString(), direction);
+            order_sql = order_sql.Replace("; ORDER", " ORDER");
+
+            // trying to select
+            try
+            {
+                table = _db.QueryList(order_sql);
+
+                _is_table_updating = false;
+                guiQueryTable_Click(null, null);
+                _is_table_updating = true;
+            }
+            catch
+            {
+                Logger.Info("No items found.");
+            }
+            finally
+            {
+                _is_table_updating = false;
+            }
+        }
+    }
+}
